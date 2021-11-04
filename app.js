062c805cc5ca01d230522abba41e7d9c0ce1451d
S545515 <charanghanta.1999@gmail.com>
2021-11-03 22:27:55 -0500

    Added

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var iphoneRouter = require('./routes/iphone');
+var addmodsRouter = require('./routes/addmods');
+
 
 var app = express();
 
@@ -20,10 +22,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.use('/iphone', iphoneRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
