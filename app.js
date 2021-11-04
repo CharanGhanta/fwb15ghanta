8e39bcbac977cd23b00f8d08e8ba9e895f40630b
S545515 <charanghanta.1999@gmail.com>
2021-11-03 21:48:31 -0500

    Added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var iphoneRouter = require('./routes/iphone');
 
 var app = express();
 
@@ -19,6 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use('/iphone', iphoneRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
